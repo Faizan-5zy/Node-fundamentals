9e96f692038928eca0814c402c3d3f35a34a76b9
Ismail Faizan <61406671+Faizan-5zy@users.noreply.github.com>
2024-01-25 17:11:30 +0530

    Create 5-data.js

---
+++ 5-make a simple API/5-data.js
@@ -0,0 +1,8 @@
+const data=[
+    {name:'Ismail Faizan',email:'fazi.com'},
+    {name:'John',email:'John.com'},
+    {name:'Peter',email:'Peter.com'},
+    {name:'SAM',email:'SAM.com'}
+] 
+
+module.exports=data
